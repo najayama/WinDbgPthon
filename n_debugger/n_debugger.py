ddf35fabd2cf0f4e4a980a01c916b2605554974c
najayama <27891506+najayama@users.noreply.github.com>
2017-11-11 19:10:46 +0900

    create min debugger

--- n_debugger/n_debugger.py
+++ n_debugger/n_debugger.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import ctypes as cty
-import my_debugger_difines as myd
+import n_debugger_difines as ndbg
 
 kernel32 = cty.windll.kernel32
 
@@ -10,5 +10,33 @@ class debugger():
         pass
     def load(self, path_to_exe):
         
+        #dwcreationflagsのための定数
+        creation_flags = CREATE_NEW_CONSOLE
+        #creation_flags = DEBUG_PROCESS
+        
+        #各構造体をインスタンス化
+        startupinfo = ndbg.STARTUPINFO()
+        process_information = PROCESS_INFOMATION()
+        
+        if kernel32.CreateProcessA(path_to_exe,
+                                   None,
+                                   None,
+                                   None,
+                                   None,
+                                   creation_flags,
+                                   None,
+                                   None,
+                                   startupinfo,
+                                   process_information):
+            print("success")
+            return 0
+        else:
+            print("something wrong")
+            return None
+        
+
+            
+                                   
+                                   
         
         
