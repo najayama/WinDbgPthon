63b047bc52cd4680811eef5969929eb26b6c083d
najayama <27891506+najayama@users.noreply.github.com>
2017-11-14 15:05:15 +0900

    Make code more readble

--- n_debugger/my_test.py
+++
@@ -1,6 +0,0 @@
-import n_debugger 
-
-debugger = n_debugger.debugger()
-
-debugger.load("bC:\\WINDOWS\\system32\\calc.exe")
-
---
+++ naja_debugger/my_test.py
@@ -0,0 +1,6 @@
+import naja_debugger
+
+debugger = naja_debugger.debugger()
+
+debugger.make_debuggee_process(b"C:\\WINDOWS\\system32\\calc.exe")
+
